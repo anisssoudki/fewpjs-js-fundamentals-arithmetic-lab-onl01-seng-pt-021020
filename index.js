98c3a56eed1ff072516dfc0575d18fcb10dc60f6
anisssoudki <aniss.marrouch@gmail.com>
2020-08-26 20:21:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+// // Write your code here
+let num1 = 31
+let num2 = 2
+
+const multiply = (num1 * num2) 
+
+const random = Math.random()
+const mod = 10 % 6  
+const max = 20
